6fd0aede3721cba9cbfbaf47cc86ae6f302352a8
JamesDansie <jdansie8336@gmail.com>
2020-06-18 14:25:37 -0700

    was getting a weird error, so copied the class code. Still had the error. Did npm install and it fixed, probably forgot to install react-redux or something.

--- redux-basics.js
+++ redux-basics.js
@@ -1,22 +1,19 @@
-// this is a practice node file.
-
 const redux = require('redux');
 const createStore = redux.createStore;
 
 const initialState = {
     counter: 0
-};
+}
 
 // Reducer
 const rootReducer = (state = initialState, action) => {
-    if(action.type === 'INC_COUNTER') {
-        // first spread the old state to make a copy, then update it. This is immutable
+    if (action.type === 'INC_COUNTER') {
         return {
             ...state,
             counter: state.counter + 1
         };
     }
-    if(action.type === 'ADD_COUNTER') {
+    if (action.type === 'ADD_COUNTER') {
         return {
             ...state,
             counter: state.counter + action.value
@@ -31,11 +28,10 @@ console.log(store.getState());
 
 // Subscription
 store.subscribe(() => {
-    console.log('[Subscription]', store.getState())
+    console.log('[Subscription]', store.getState());
 });
 
-// Action
+// Dispatching Action
 store.dispatch({type: 'INC_COUNTER'});
 store.dispatch({type: 'ADD_COUNTER', value: 10});
 console.log(store.getState());
-
